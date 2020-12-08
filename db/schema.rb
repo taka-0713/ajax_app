381c1b5dc15aca459b49eb5ed43da1145ce663de
taka-0713 <ottake07@gmail.com>
2020-12-08 19:35:05 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_16_053948) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.boolean "checked"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
